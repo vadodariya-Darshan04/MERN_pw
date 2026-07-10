d260be49c98ac6282f1314b7618a814b898fbc2d
Darshan Vadodariya <drvadodariya10@gmail.com>
2026-07-10 19:37:13 +0530

    Q 4 to 7 added

---
+++ Assignment_1/checkNumber.js
@@ -0,0 +1,16 @@
+function checkNumber() {
+
+
+    let num = 10;
+    if (num > 0)
+        console.log("Number is positive");
+        
+    else if (num < 0)
+        console.log("Number is Negative");
+
+    else
+        console.log("Number is Zero");
+
+    }
+
+checkNumber();
